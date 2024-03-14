112ca1e74a0e888bc7e665987855020632575ff8
Kishore174 <skishore174@gmail.com>
2024-03-14 17:01:36 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import "./Script";
+import "./MediaQuery.css"
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+  <div>
+    <Home/>
+  </div>
   );
 }
 
